b4e45773b9950bfff737194a695f88db33532a24
Pascal Deschênes <pdeschen@rassemblr.com>
2011-07-18 18:31:47 -0400

    more test cases

--- test.js
+++ test.js
@@ -1,21 +1,44 @@
-var Camelot = require('./camelot.js');
+var Camelot = require('./camelot.js'), uuid = require('node-uuid');
 
 var camelot = new Camelot( {
   'rotate' : '180',
-  'banner' : {'position': 'top', 'color': '#00000000', 'line': '#ffffffff'},
-  'text-color' : '#ffffffff',
-  
   'flip' : 'v'
-    });
-
+});
+var focus = 10;
+var brightness = -128;
 camelot.on('frame', function (image) {
   console.log('frame received!');
+  var name = uuid();
+  require('fs').writeFile('/tmp/test/' + name + '.png', image, function (err) {
+    if (err)
+      throw err;
+    console.log('It\'s saved!');
+  });
+  focus += 10;
+  brightness += 10;
+  camelot.update( {
+    'title' : name,
+    'controls' : {
+      'focus' : focus,
+      'brightness' : brightness
+    }
+  });
 });
 
 camelot.grab( {
-      'title' : 'Camelot',
+  'title' : 'Camelot',
   'font' : 'Arial:24',
-  'frequency' : 1,    
-}, function (frame) {
-  console.log('frame callback');
+  'frequency' : 1,
+  'controls' : {
+    'focus' : 'auto'
+  }
 });
+
+// camelot.update( {
+// 'title' : 'toto'
+// });
+
+/*
+ * , 'banner' : {'position': 'top', 'color': '#00000000', 'line': '#ffffffff'},
+ * 'text-color' : '#ffffffff',
+ */
